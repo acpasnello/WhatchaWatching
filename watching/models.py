cd184465074144b867934c7e6f23841fddd5b850
acpasnello <anthony.pasnello@gmail.com>
2023-02-18 14:20:30 -0500

    Add ListItem model string display

--- watching/models.py
+++ watching/models.py
@@ -26,6 +26,9 @@ class ListItem(models.Model):
     type = models.CharField(max_length=20, choices=media_types)
     poster = models.CharField(max_length=200, null=True)
 
+    def __str__(self):
+        return '%s on %s of %s' % (self.name, self.list.name, self.list.owner.username)
+
 class Media(models.Model):
     # Define media type options
     MOVIE = 'M'
