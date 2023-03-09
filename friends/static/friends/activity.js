e3caef0de2bbcb94459e0d5e3678cd37f0291fa7
acpasnello <anthony.pasnello@gmail.com>
2023-03-09 18:29:08 -0500

    Accept changes in activity display.

--- friends/static/friends/activity.js
+++ friends/static/friends/activity.js
@@ -21,52 +21,32 @@ function activityBlock(activity, displayDiv) {
     if (activity.action == "reviewed") {
         newDiv.innerHTML = 
         `<div>
-<<<<<<< HEAD
             <a href="/watching/community/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="${activity.medialink}" class="browseList">${activity.name}</a>.<br>
-=======
-            <a href="/watching/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="${activity.medialink}" class="browseList">${activity.name}</a>.<br>
->>>>>>> 586772be6bba14d659d948b3dafac54f3713dd03
             <b>${activity.rating}</b>: "${activity.review}"
         </div>`
     }
     else if (activity.action == "rated") {
         newDiv.innerHTML = 
         `<div>
-<<<<<<< HEAD
             <a href="/watching/community/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="${activity.medialink}" class="browseList">${activity.name}</a>: <b>${activity.rating}</b>
-=======
-            <a href="/watching/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="${activity.medialink}" class="browseList">${activity.name}</a>: <b>${activity.rating}</b>
->>>>>>> 586772be6bba14d659d948b3dafac54f3713dd03
         </div>`
     }
     else if (activity.action == "added") {
         newDiv.innerHTML = 
         `<div>
-<<<<<<< HEAD
             <a href="/watching/community/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="${activity.medialink}" class="browseList">${activity.listitem}</a> to their list, <a href="${activity.listlink}">${activity.list}</a>.
-=======
-            <a href="/watching/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="${activity.medialink}" class="browseList">${activity.listitem}</a> to their list, <a href="${activity.listlink}">${activity.list}</a>.
->>>>>>> 586772be6bba14d659d948b3dafac54f3713dd03
         </div>`
     }
     else if (activity.action == "watched") {
         newDiv.innerHTML = 
         `<div>
-<<<<<<< HEAD
             <a href="/watching/community/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="#" class="browseList">${activity.listitem}.
-=======
-            <a href="/watching/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} <a href="#" class="browseList">${activity.listitem}.
->>>>>>> 586772be6bba14d659d948b3dafac54f3713dd03
         </div>`
     }
     else if (activity.action == "created") {
         newDiv.innerHTML = 
         `<div>
-<<<<<<< HEAD
             <a href="/watching/community/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} a new list, <a href="#">${activity.list}</a>.
-=======
-            <a href="/watching/profile/${activity.userID}" class="friendsresult">${activity.user}</a> ${activity.action} a new list, <a href="#">${activity.list}</a>.
->>>>>>> 586772be6bba14d659d948b3dafac54f3713dd03
         </div>`
     }
     
